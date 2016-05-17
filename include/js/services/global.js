ff1d673a913fa62639806fde0d5e8b32acd28084
Angel Carvajal <angel_adcm@hotmail.com>
2016-05-17 15:51:31 -0500

    Feat: Add global variables factory

---
+++ include/js/services/global.js
@@ -0,0 +1,13 @@
+angular.module("global",[])
+.factory("globalVariables",function(){
+	//Local variables declaration
+	var data={};
+	var DS = "/";
+	var URL =window.location.protocol+DS+DS+window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+	var ROOT = DS+"CV"+DS+"app"+DS;
+	
+	//Complex object to return declaration
+	data.hostname=URL+ROOT;
+
+	return data;
+})
