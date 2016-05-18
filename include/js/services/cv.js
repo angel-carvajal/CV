26919cb1d46e7d40b5c68803defbb07fbc75da0d
Angel Carvajal <angel_adcm@hotmail.com>
2016-05-18 15:49:27 -0500

    Feat: Add main structure of CV factories to use

---
+++ include/js/services/cv.js
@@ -0,0 +1,67 @@
+angular.module("cv",[])
+
+.factory("generalContent",["globalVariables",function(globalVariables){
+	//Variables declaration
+
+	//Content definition
+	data={
+		
+	};
+
+	return data;
+}])
+.factory("knowledgeContent",["globalVariables",function(globalVariables){
+	//Variables declaration
+	var data={};
+
+	//Content definition
+	data={
+		
+	};
+
+	return data;
+}])
+.factory("coursesContent",["globalVariables",function(globalVariables){
+	//Variables declaration
+	var data={};
+
+	//Content definition
+	data={
+		
+	};
+
+	return data;
+}])
+.factory("experienceContent",["globalVariables",function(globalVariables){
+	//Variables declaration
+	var data={};
+
+	//Content definition
+	data={
+		
+	};
+
+	return data;
+}])
+.factory("projectsContent",["globalVariables",function(globalVariables){
+	//Variables declaration
+	var data={};
+
+	//Content definition
+	data={
+		
+	};
+
+	return data;
+}])
+.factory("extrasContent",["globalVariables",function(globalVariables){
+	//Variables declaration
+	var data={};
+
+	//Content definition
+	data={
+		
+	};
+
+	return data;
+}])
