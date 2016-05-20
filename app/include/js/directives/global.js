c7cb64be3d31721b7eb60594c66c01927ba280cc
Angel Carvajal <angel_adcm@hotmail.com>
2016-05-20 11:38:32 -0500

    Fix: Bug on loading welcome. Feat: Lazy Load styles

--- app/include/js/directives/global.js
+++ app/include/js/directives/global.js
@@ -64,6 +64,57 @@ angular.module("globalDirectives",[])
 	//console.log(globalVariables.hostname);
 	return{
 		restrict:"E",
+		scope:{
+			load:"="
+		},
+		controller:["$scope","$timeout",function($scope,$timeout){
+			$timeout(function(){
+				//Typed effect				
+				$(function(){
+					$(".typing").typed({
+						strings: [""+$scope.load.title.title.text+""],
+						typeSpeed: 50,
+						showCursor: false,
+						startDelay: 1000,
+						callback: function() {
+							//Add class to simulate move
+							$(".welcome__image__container").addClass("welcome__image__container--actived");
+							//Listener of transition - standard
+							$(".welcome__image__container")[0].addEventListener("transitionend",function(){
+								$timeout(function(){
+									$(".welcome__load").addClass("welcome__load--actived");
+
+									$(".welcome__load")[0].addEventListener("webkitTransitionEnd",function(){
+										$scope.updateLoad();
+									});
+									$(".welcome__load")[0].addEventListener("transitionend",function(){
+										$scope.updateLoad();
+									});
+								},1500);
+							});
+							//Listener of transition - Safari 3.1 to 6.0
+							$(".welcome__image__container")[0].addEventListener("webkitTransitionEnd",function(){
+								$timeout(function(){
+									$(".welcome__load").addClass("welcome__load--actived");
+
+									$(".welcome__load")[0].addEventListener("webkitTransitionEnd",function(){
+										$scope.updateLoad();
+									});
+									$(".welcome__load")[0].addEventListener("transitionend",function(){
+										$scope.updateLoad();
+									});
+								},1500);
+							});
+						}
+					});
+				});
+			},0);
+
+			$scope.updateLoad=function(){
+				$scope.$parent.$parent.load=true;
+				$scope.$parent.$parent.$apply();
+			};
+		}],
 		templateUrl:globalVariables.hostname+"include/templates/global/loading.html"
 	};
 }])
