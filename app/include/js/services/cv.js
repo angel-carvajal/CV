aa55a879a912431c05f095abb2eb0bda38139e4c
Angel Carvajal <angel_adcm@hotmail.com>
2016-05-21 16:17:49 -0500

    Feat: Add content and structure to Footer info directive

--- app/include/js/services/cv.js
+++ app/include/js/services/cv.js
@@ -34,10 +34,6 @@ angular.module("cv",[])
 .factory("generalContent",["globalVariables",function(globalVariables){
 	//Variables declaration
 	var data={};
-	var iconColor="teal";
-	var iconType="flat";
-	var iconSize="s";
-	var iconClass="general-info__contact__option__title__icon";
 	var age=(Date.now()-(new Date("1991,4,8")));
 	age=new Date(age); //move this to a filter
 	age=age.getUTCFullYear()-1970;
@@ -188,10 +184,189 @@ angular.module("cv",[])
 .factory("footerContent",["globalVariables",function(globalVariables){
 	//Variables declaration
 	var data={};
+	var contactIconColor="white";
+	var contactIconType="flat";
+	var contactIconSize="m";
+	var contactIconClass="footer__contact__option__title__icon";
+
+	var socialIconColor="white";
+	var socialIconType="flat";
+	var socialIconSize="m";
+	var socialIconClass="footer__social__option__title__icon";
 
 	//Content definition
 	data={
-		
+		title:{
+			title:{
+				title:{
+					text: 	"Contacto",
+					class: 	"footer__title__title"
+				},
+				class: 	"footer__title__title__container"
+			},
+			paragraph:{
+				title:{
+					text: 	"\"Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.\"",
+					class: 	"footer__title__paragraph"
+				},
+				class: 	"footer__title__paragraph__container"
+			},
+			author:{
+				title:{
+					text: 	"Samuel Beckett",
+					class: 	"footer__title__author"
+				},
+				class: 	"footer__title__author__container"
+			},
+			class: 	"footer__title"
+		},
+		contact:{
+			options:[
+				{
+					title:{
+						title:{
+							text: 	"866 100 7948",
+							class: 	"footer__contact__option__title"
+						},
+						class: 	"footer__contact__option__title__container"
+					},
+					icon:{
+						id: 	"cellphone-android",
+						size: 	contactIconSize,
+						color: 	contactIconColor,
+						type: 	contactIconType,
+						class: 	contactIconClass
+					},
+					class: 	"footer__contact__option"
+				},
+				{
+					title:{
+						title:{
+							text: 	"angel_adcm@hotmail.com",
+							class: 	"footer__contact__option__title"
+						},
+						class: 	"footer__contact__option__title__container"
+					},
+					icon:{
+						id: 	"email",
+						size: 	contactIconSize,
+						color: 	contactIconColor,
+						type: 	contactIconType,
+						class: 	contactIconClass
+					},
+					class: 	"footer__contact__option"
+				},
+				{
+					title:{
+						title:{
+							text: 	"angel_adcm",
+							class: 	"footer__contact__option__title"
+						},
+						class: 	"footer__contact__option__title__container"
+					},
+					icon:{
+						id: 	"skype",
+						size: 	contactIconSize,
+						color: 	contactIconColor,
+						type: 	contactIconType,
+						class: 	contactIconClass
+					},
+					class: 	"footer__contact__option"
+				},
+			],
+			class:"footer__contact"
+		},
+		social:{
+			options:[
+				{
+					title:{
+						title:{
+							text: 	"@angdanadcm",
+							class: 	"footer__social__option__title"
+						},
+						class: 	"footer__social__option__title__container"
+					},
+					link:{
+						href: 	"https://twitter.com/angdanadcm",
+						target: "_blank",
+						class: 	"footer__social__option__link"
+					},
+					icon:{
+						id: 	"twitter",
+						size: 	socialIconSize,
+						color: 	socialIconColor,
+						type: 	socialIconType,
+						class: 	socialIconClass
+					},
+					class: 	"footer__social__option"
+				},
+				{
+					title:{
+						title:{
+							text: 	"angel-carvajal-6b853585",
+							class: 	"footer__social__option__title"
+						},
+						class: 	"footer__social__option__title__container"
+					},
+					link:{
+						href: 	"https://mx.linkedin.com/in/angel-carvajal-6b853585",
+						target: "_blank",
+						class: 	"footer__social__option__link"
+					},
+					icon:{
+						id: 	"linkedin",
+						size: 	socialIconSize,
+						color: 	socialIconColor,
+						type: 	socialIconType,
+						class: 	socialIconClass
+					},
+					class: 	"footer__social__option"
+				},
+				{
+					title:{
+						title:{
+							text: 	"angeldaniel-adcm",
+							class: 	"footer__social__option__title"
+						},
+						class: 	"footer__social__option__title__container"
+					},
+					link:{
+						href: 	"https://github.com/angeldaniel-adcm",
+						target: "_blank",
+						class: 	"footer__social__option__link"
+					},
+					icon:{
+						id: 	"github-circle",
+						size: 	socialIconSize,
+						color: 	socialIconColor,
+						type: 	socialIconType,
+						class: 	socialIconClass
+					},
+					class: 	"footer__social__option"
+				}
+			],
+			class:"footer__social"
+		},
+		signature:{
+			title:{
+				title:{
+					text: 	"Made by",
+					class: 	"footer__signature__title"
+				},
+				class: 	"footer__signature__title__container"
+			},
+			logo:{
+				image:{
+					src: 	"ac_logo_1.2.png",
+					class: 	"footer__signature__logo"
+				},
+				class: 	"footer__signature__logo__container"
+			},
+			host: 	globalVariables.hostname,
+			class: 	"footer__signature"
+		},
+		classToAnimate: "bounceInUp",
+		class:"footer"
 	};
 
 	return data;
